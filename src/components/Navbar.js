0ac4110d0a59558a52d931ff950197cbc4f43546
Kavin <kavinchai00@gmail.com>
2023-12-02 14:05:22 -0500

    Added navbar from v2

---
+++ src/components/Navbar.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "../css/Navbar.css";
+import {scrollTo} from "../helper";
+
+const Navbar = () => {
+  const [myName, setMyName] = useState(true);
+
+  return (
+    <div className="navBarComponent">
+      <div className="navBarContainer">
+        <div
+          className={myName === true ? "firstName firstNameEng navName" : "firstName firstNameTh navName"}
+          onClick={() => setMyName((prev) => !prev)}
+        >
+          {myName === true ? "kavin" : "กวิน"}
+        </div>
+        <div
+          className={myName === true ? "lastName lastNameEng navName" : "lastName lastNameTh navName"}
+          onClick={() => setMyName((prev) => !prev)}
+        >
+          {myName === true ? "chai" : "ฉาย"}
+        </div>
+        <div className="navMenu">
+          <button
+            className="navBtn"
+            onClick={() => scrollTo(".aboutComponent")}
+          >
+            about
+          </button>
+          <button className="navBtn" onClick={() => scrollTo(".exp-component")}>
+            experience
+          </button>
+          <button
+            className="navBtn"
+            onClick={() => scrollTo(".projectComponent")}
+          >
+            projects
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
