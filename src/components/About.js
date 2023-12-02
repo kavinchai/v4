782e91fce7dbdeb1b62df658f892cbeead243c62
Kavin <kavinchai00@gmail.com>
2023-12-02 14:04:53 -0500

    Created About Section

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,32 @@
 import React from "react";
-import "../css/About.css"
+import { TypeAnimation } from "react-type-animation";
+import "../css/About.css";
 
 const About = () => {
-    return (
-        <div className="aboutComponent">
-            <div className="aboutContainer"></div>
+
+  return (
+    <div className="about-component">
+        <div className="left-section">
+          <text className="name-section">
+            Kavin Chaisawangwong
+          </text>
+          <TypeAnimation
+            sequence ={[
+              "I'm a software engineer",
+              1000,
+              "I'm a photographer",
+              1000,
+              "I'm a designer",
+              1000
+            ]}
+            speed={30}
+            repeat={Infinity}
+            className="desc-section"
+          />
         </div>
-    )
+        <div className="right-section"></div>
+    </div>
+  );
 };
 
 export default About;
