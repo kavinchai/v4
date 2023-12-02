60df6f2e00dde3f2e7c6e6337625c2986d0b6181
Kavin <kavinchai00@gmail.com>
2023-12-02 14:04:42 -0500

    Restructured App

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import About from "./About"
+import React from "react";
+
+import Navbar from "./Navbar";
+import About from "./About";
+import Exp from "./Exp";
 
 const App = () => {
   return (
     <>
+    
+      {/* <Navbar/> */}
       <About/>
+      <Exp/>
     </>
   );
-}
+};
 
 export default App;
