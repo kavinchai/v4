c0aba4df458ea100b69981c698368d80233aa3b0
Kavin <kavinchai00@gmail.com>
2023-12-02 14:04:32 -0500

    Added from v2

---
+++ src/constants.js
@@ -0,0 +1,13 @@
+const IMAGES = {
+  logoArduino: require("./imgs/techImgs/logoArduino.png"),
+  logoC: require("./imgs/techImgs/logoC.png"),
+  logoCss: require("./imgs/techImgs/logoCss.png"),
+  logoDocker: require("./imgs/techImgs/logoDocker.png"),
+  logoHtml: require("./imgs/techImgs/logoHtml.png"),
+  logoJs: require("./imgs/techImgs/logoJs.png"),
+  logoLiquibase: require("./imgs/techImgs/logoLiquibase.png"),
+  logoPython: require("./imgs/techImgs/logoPython.png"),
+  logoReact: require("./imgs/techImgs/logoReact.png"),
+  logoSpring: require("./imgs/techImgs/logoSpring.png"),
+};
+export default IMAGES;
