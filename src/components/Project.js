8743e9d9278a7b97351ba39db9e5abbdd2a595a6
Kavin <kavinchai00@gmail.com>
2023-12-04 09:12:27 -0500

    Created project

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,19 +5,20 @@ import projectsJson from "../content/projects.json";
 
 const Project = () => {
   return (
-    <div className="projectComponent">
-      <div className="projectContainer">
-        <div className="projectHeader">
-          <div className="projTxt1">projects</div>
-          <div className="projTxt2">things i've made</div>
-        </div>
-        <div className="projectBody">
+    <>
+    <div className="project-trans"/>
+    <div className="project-component">
+      <div className="project-container">
+        <div className="project-header project-header-text">Projects</div>
+        <div className="project-header-subtitle">Things i've made</div>
+        <div className="project-body">
           {projectsJson.data.map((projInfo, index) => (
             <ProjectCard key={index} proj={projInfo}/>
           ))}
         </div>
       </div>
     </div>
+  </>
   );
 };
 
--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,26 +4,26 @@ import "../css/ProjectCard.css";
 const ProjectCard = ({ proj: { title, desc, link, tech } }) => {
   return (
     <a
-      className="projCardComponent"
+      className="project-card-component"
       href={link}
       target="_blank"
       rel="noreferrer"
     >
-      <div className="projHeader">
-        <div className="projHeaderSect">
-          <div className="projTitle">{title}</div>
+      <div className="project-card-header">
+        <div className="project-card-header-sect">
+          <div className="project-card-title">{title}</div>
         </div>
       </div>
-      <div className="projBody">
-        <div className="projBodySect"> 
-          <div className="projDesc">{desc}</div>
+      <div className="project-card-body">
+        <div className="project-card-body-sect"> 
+          <div className="project-card-desc">{desc}</div>
         </div>
       </div>
-      <div className="projFooter">
-        <div className="projFooterSect">
-          <div className="projTech">
+      <div className="project-card-footer">
+        <div className="project-card-footer-sect">
+          <div className="project-card-tech">
             {tech.map((key, index) => (
-              <div key={index} className="techUsed">
+              <div key={index} className="tech-used">
                 {key}
               </div>
             ))}
