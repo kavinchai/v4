366c20a167e71c473b5916fab3399efcadab6549
Kavin <kavinchai00@gmail.com>
2023-12-04 09:12:01 -0500

    Changed About to FirstPage and created About

--- src/components/About.js
+++ src/components/About.js
@@ -1,33 +1,17 @@
 import React from "react";
-import { TypeAnimation } from "react-type-animation";
 import "../css/About.css";
 
 const About = () => {
-
   return (
-    <div className="about-component">
-        <div className="left-section">
-          <text className="name-section">
-            Kavin Chaisawangwong
-          </text>
-          <TypeAnimation
-            sequence ={[
-              "I write code",
-              1000,
-              "I design websites",
-              1000,
-              "I take photos",
-              1000,
-              "I review coffee",
-              1000
-            ]}
-            speed={30}
-            repeat={Infinity}
-            className="desc-section"
-          />
+    <>
+        <div className="about-trans"/>
+        <div className="about-component">
+            <div className="about-container">
+                
+            </div>
         </div>
-        <div className="right-section"></div>
-    </div>
+    </>
+    
   );
 };
 
